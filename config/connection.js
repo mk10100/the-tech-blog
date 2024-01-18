762c0abd22df67dd6a5dbb3ee329dd8cb6fb47c4
mk10100 <mohamedkhalil10100@gmail.com>
2024-01-17 19:43:04 -0500

    added scripts

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,12 @@ require("dotenv").config();
 // Create a new Sequelize instance
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Important - set this to false
+    },
+  },
 });
 
 module.exports = sequelize;
